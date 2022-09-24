81a0ff15c8271d4be46bc95174d6e095069185ef
AeKa <a_kasyanov@rambler.ru>
2022-09-24 15:26:20 +0300

    Create Car.class

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,57 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long carId;
+    @Column(model = "model")
+    private String model;
+    @Column(series = "series")
+    int series;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        if(this == null) {
+            return "the car was not found in the database.";
+        }
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
